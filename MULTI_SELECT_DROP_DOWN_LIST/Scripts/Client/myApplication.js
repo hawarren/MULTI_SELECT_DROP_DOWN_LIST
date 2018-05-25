d7381068cdef61f71c575aed6912970dd5adcac4
Hanif Warren <hanif.warren@gmail.com>
2018-05-25 02:14:02 -0400

    midway thru refactoring code so it's not all on one page

--- MULTI_SELECT_DROP_DOWN_LIST/Scripts/Client/myApplication.js
+++ MULTI_SELECT_DROP_DOWN_LIST/Scripts/Client/myApplication.js
@@ -1,1 +1,5 @@
-﻿
+﻿(function () {
+    //make sure this matches the ng-app in the view
+    var app = angular.module("myApplication", ["MyController", "myDirectivesApplication"]);
+
+}())
--- MULTI_SELECT_DROP_DOWN_LIST/Scripts/Client/myController.js
+++ MULTI_SELECT_DROP_DOWN_LIST/Scripts/Client/myController.js
@@ -1,1 +1,40 @@
-﻿
+﻿(function (app)
+{
+    //registers the controller with the main module
+
+    var app = angular.module("myApplication");
+    //capitalized to indicate constructor function
+    var MyController = function ($scope) {
+
+
+			$scope.users = [
+				{ "id": 1, "name": "Ali" },
+				{ "id": 2, "name": "Sara" },
+				{ "id": 3, "name": "Babak" },
+				{ "id": 4, "name": "Sanaz" },
+				{ "id": 5, "name": "Dariush" },
+			];
+
+			$scope.selectedUserIds = [3, 5];
+
+    };
+    app.controller("MyController", MyController);
+
+
+    //put directive here for now
+
+});
+
+//varMyApplication.controller('MyController', function ($scope) {
+
+//			$scope.users = [
+//				{ "id": 1, "name": "Ali" },
+//				{ "id": 2, "name": "Sara" },
+//				{ "id": 3, "name": "Babak" },
+//				{ "id": 4, "name": "Sanaz" },
+//				{ "id": 5, "name": "Dariush" },
+//			];
+
+//			$scope.selectedUserIds = [3, 5];
+
+//		});
